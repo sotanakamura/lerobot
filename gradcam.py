b00be7ed960034c5126ec1d05e3f4846cd97080b
Sota Nakamura <sota.nakamura@rt-net.jp>
2026-03-16 14:02:57 +0900

    add animation

--- gradcam.py
+++ gradcam.py
@@ -36,45 +36,55 @@ from lerobot.policies.utils import (
     populate_queues,
 )
 
-dataset = LeRobotDataset("", "/home/nakamura/data/tomato_merged_20260225/user/id")
+dataset = LeRobotDataset("", "/home/nakamura/data/tomato_merged_20260225/user/id", episodes=[0])
 policy = DiffusionPolicy.from_pretrained("/home/nakamura/outputs/train/diffusion_20260225/checkpoints/200000/pretrained_model")
-policy.cpu()
+policy.cuda()
 policy.train()
 dataloader = torch.utils.data.DataLoader(dataset)
-batch = next(iter(dataloader))
+
 visualizers = []
 for encoder in policy.diffusion.rgb_encoder:    
     target_layer = encoder.backbone[-1]
     visualizer = GradCAMVisualizer(target_layer)
     visualizers.append(visualizer)
 
-batch[OBS_IMAGES] = torch.stack([batch[key] for key in policy.config.image_features], dim=-4)
-policy._queues = populate_queues(policy._queues, batch)
-actions = policy.predict_action_chunk(batch)
-target = torch.sum(torch.abs(actions[0,0]))
+im_color = None
+im_heatmap = None
 
-policy.zero_grad()
-target.backward()
+for batch in dataloader:
+    batch = {key: value.cuda() for key, value in batch.items() if isinstance(value, torch.Tensor)}
+    batch[OBS_IMAGES] = torch.stack([batch[key] for key in policy.config.image_features], dim=-4)
+    policy._queues = populate_queues(policy._queues, batch)
+    actions = policy.predict_action_chunk(batch)
+    target = torch.sum(torch.abs(actions[0,0]))
 
-images = [image[0] for key, image in batch.items() if key.startswith("observation.images.") and "eye" not in key]
-heatmaps = [visualizer.generate_heatmap()[0] for visualizer in visualizers]
+    policy.zero_grad()
+    target.backward()
 
-colored_images = []
-resized_heatmaps = []
+    images = [batch[key][0].cpu() for key in policy.config.image_features]
+    heatmaps = [visualizer.generate_heatmap()[0].cpu() for visualizer in visualizers]
 
-for image, heatmap in zip(images, heatmaps):
-    image = image.cpu().numpy().transpose(1, 2, 0)
-    heatmap = heatmap.unsqueeze(0).unsqueeze(0)
-    heatmap = F.interpolate(heatmap, size=(image.shape[0], image.shape[1]), mode='bilinear', align_corners=False).squeeze().numpy()
-    colored_images.append(image)
-    resized_heatmaps.append(heatmap)
+    colored_images = []
+    resized_heatmaps = []
+
+    for image, heatmap in zip(images, heatmaps):
+        image = image.cpu().numpy().transpose(1, 2, 0)
+        heatmap = heatmap.unsqueeze(0).unsqueeze(0)
+        heatmap = F.interpolate(heatmap, size=(image.shape[0], image.shape[1]), mode='bilinear', align_corners=False).squeeze().numpy()
+        colored_images.append(image)
+        resized_heatmaps.append(heatmap)
 
 
-image = np.concatenate(colored_images,axis=1)
-image = np.mean(image, axis=2)
-heatmap = np.concatenate(resized_heatmaps,axis=1)
+    image = np.concatenate(colored_images,axis=1)
+    image = np.mean(image, axis=2)
+    heatmap = np.concatenate(resized_heatmaps,axis=1)
 
-plt.imshow(image, cmap='gray')
-plt.imshow(heatmap, cmap='jet', alpha=0.5)
-plt.axis('off')
-plt.show()
+    if im_color is None:
+        im_color = plt.imshow(image, cmap='gray')
+        im_heatmap = plt.imshow(heatmap, cmap='jet', alpha=0.5)
+    else:
+        im_color.set_data(image)
+        im_heatmap.set_data(heatmap)
+    plt.axis('off')
+    plt.draw()
+    plt.pause(0.03)
